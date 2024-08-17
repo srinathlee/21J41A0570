22921a2a011e9cac253b2134a69a7b23622125a2
srinath5255 <iamsrinath5255@gmail.com>
2024-08-17 12:20:32 +0530

    added product router in server and created cards in client

--- client/src/components/products.js
+++ client/src/components/products.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import {React,useEffect,useState} from 'react';
 import ProductCard from './product';
+import axios from "axios"
 
 // Example product data
 const products = [
@@ -27,14 +28,34 @@ const products = [
 ];
 
 
-const ProductPage = () => {
+const ProductPage = async() => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+   
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/');
+        // setData(response); 
+        // setLoading(false); 
+        console.log(response)
+      } catch (err) {
+        // console.log(err)
+        // setLoading(false); 
+        console.log(err)
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Product List</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {products.map(product => (
+        {/* {products.map(product => (
           <ProductCard key={product.id} product={product} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,47 @@
-exports.getallProducts = (req, res) => {
-    res.send('User Controller Function');
-};
+const axios = require('axios');
+
+
+
+exports.getallProducts = async (req, res) => {
+    try {
+
+    const authBody={
+        "companyName": "goMart",
+        "clientID": "5a89e05d-e121-47dc-9463-4809c731786b",
+        "clientSecret": "UaEPcgMFnxMdPIIT",
+        "ownerName": "B Srinath",
+        "ownerEmail": "iamsrinath5255@gmail.com",
+        "rollNo": "21J41A0570"
+      }
+
+      const authUrl="http://20.244.56.144/test/auth"
+
+      const authResponse=await axios.post(authUrl,authBody)
+      const access_token=authResponse.data.access_token
+
+
+    
+      const headers = {
+        "Authorization": `Bearer ${access_token}`, 
+        'Content-Type': 'application/json' 
+      };
+  
+      // Make the request with headers
+      const response = await axios.get(
+        'http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=100&maxPrice=100000',
+        {headers} // Correctly pass the headers in the configuration object
+      );
+  
+ 
+      console.log(response.data); 
+      res.json(response.data)
+
+    } catch (e) {
+      // Handle errors
+      console.error(e);
+      res.status(500).json({ error: 'Failed to fetch products' });
+    }
+  };
 
 // Function to handle login (if needed)
 exports.getSpecificProduct = (req, res) => {
